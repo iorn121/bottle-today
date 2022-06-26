068d92fcbd60ba748305847281089dcca30681e7
Io <wmt.tkn.121@gmail.com>
2022-06-26 23:55:37 +0900

    sankou

--- today_api.py
+++ today_api.py
@@ -9,4 +9,6 @@ print(today)
 day_url='%s月%s日'%(today.month,today.day)
 print(day_url)
 
+# https://www.autumn-color.com/archives/24
+
 base_url='https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&exintro&explaintext&redirects=1&titles='
