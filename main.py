6290450bce7c3084894d71f0a00905cddb5cbc29
Io <wmt.tkn.121@gmail.com>
2022-07-12 00:09:08 +0900

    hello world

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import functools
+def application(env,start_response):
+    start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
+    return [b"Hello, world!"]
 
-
-class lazy_attribute:
-    """A property that caches itself to the class object
-
-    Returns:
-        _type_: _description_
-    """
-    def __init__(self,func):
-        functools.update_wrapper(self.func,updated=[])
-        self.generic=func
-
-    def __get__(self,obj,cls):
-        value=self.getter(cls)
-        setattr(cls,self.__name__,value)
-        return value
-
-class Glass:
-
-    @lazy_attribute
-    def _global_config(cls):
-        cfg = ConfigDict()
-        cfg.meta_set("catchall","validate",bool)
-        return cfg
-    
-    def __init__(self,**kwargs):
-        self.config =self._global_config._make_overlay()
+# https://c-bata.link/webframework-in-python/
