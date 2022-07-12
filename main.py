d2d132f6009da685f03043ac479931cc0fe5e762
Io <wmt.tkn.121@gmail.com>
2022-07-13 01:06:25 +0900

    simple router

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-def application(env,start_response):
-    start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
-    return [b"Hello, world!"]
-
 # https://c-bata.link/webframework-in-python/
+
+
+def application(env,start_response):
+    path=env["PATH_INFO"]
+    if path=="/":
+        start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
+        return [b"Hello, world!"]
+
+    elif path=="/foo":
+        start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
+        return [b"foo"]
+
+class Router:
+    def __init__(self):
+        self.routes = []
+
+    def add(self,method,path,callback):
+        self.routes.append({
+            "method":method,
+            "path":path,
+            "callback":callback
+        })
+    def match(self,method,path):
+        for r in self.routes:
+            matched=re.compile(r["path"]).match(path)
+            if matched and r["method"]==method:
+                url_vars=matched.groupdict()
+                return r["callback"], url_vars
+        return http404,{}
+
+import re
+
+def http404(env,start_response):
+    start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
+    return [b"404 Not Found"]
+    
